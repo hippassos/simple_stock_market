4ec60aa68d392008ab4202e7fe132e2a81067b12
George Lois <georgelois89@gmail.com>
2025-03-29 08:56:00 +0200

    feat: add trade endpoint to buy and sell
    chore: apply
    double quotes
    
    refactor: rewrite the log message when a stock is not found

--- app/stocks/routes.py
+++ app/stocks/routes.py
@@ -2,7 +2,7 @@ import logging
 from app.stocks import bp
 from flask import request
 from app.stocks.models import Stock, StockMarket
-from app.stocks.decorators import price_validation
+from app.stocks.decorators import numbers_validated, trade_type_validation
 from app.stocks.errors import error_response
 
 available_stocks = {
@@ -16,7 +16,7 @@ available_stocks = {
 stock_market = StockMarket([], available_stocks)
 
 
-@bp.route('/list_stocks', methods=['GET'])
+@bp.route("/list_stocks", methods=["GET"])
 def list_stocks():
     stocks = stock_market.list_stocks()
     return {
@@ -24,15 +24,23 @@ def list_stocks():
     }
 
 
-@bp.route('/dividend_yield', methods=['GET'])
-@price_validation
+@bp.route("/list_trades", methods=["GET"])
+def list_trades():
+    trades = stock_market.list_trades()
+    return {
+        "trades": [t.__dict__ for t in trades]
+    }
+
+
+@bp.route("/dividend_yield", methods=["GET"])
+@numbers_validated(["price"])  # input validation
 def dividend_yield():
-    symbol = request.args.get('symbol')
-    price = int(request.args.get('price', 0))
+    symbol = request.args.get("symbol")
+    price = int(request.args.get("price", 0))
     stock = stock_market.get_stock(symbol)
     if not stock:
-        # keep track for future analytics for client needs
-        logging.error(f"Not available stock requested with symbol: {symbol}")
+        # keep track to understand if there is trend on stocks requested that are not available
+        logging.error(f"Stock requested is not available, {symbol}")
         return error_response(404, "Stock not found")
     logging.info(f"Calculating dividend yield for {symbol} at price {price}")
     return {
@@ -40,15 +48,14 @@ def dividend_yield():
     }
 
 
-@bp.route('/pe_ratio', methods=['GET'])
-@price_validation
+@bp.route("/pe_ratio", methods=["GET"])
+@numbers_validated(["price"])  # input validation
 def pe_ratio():
-    symbol = request.args.get('symbol')
-    price = int(request.args.get('price', 0))
+    symbol = request.args.get("symbol")
+    price = int(request.args.get("price", 0))
     stock = stock_market.get_stock(symbol)
     if not stock:
-        # keep track for future analytics for client needs
-        logging.error(f"Not available stock requested with symbol: {symbol}")
+        logging.error(f"Stock requested is not available, {symbol}")
         return error_response(404, "Stock not found")
     logging.info(f"Calculating P/E ratio for {symbol} at price {price}")
     return {
@@ -56,21 +63,34 @@ def pe_ratio():
     }
 
 
-@bp.route('/trade', methods=['POST'])
+@bp.route("/trade", methods=["POST"])
+@numbers_validated(["price", "quantity"])  # input validation
+@trade_type_validation
 def trade():
+    data = request.get_json()
+    symbol = data.get("symbol")
+    quantity = data.get("quantity")
+    price = data.get("price")
+    trade_type = data.get("type")
+
+    stock = stock_market.get_stock(symbol)
+    if not stock:
+        logging.error(f"Stock requested is not available, {symbol}")
+        return error_response(404, "Stock not found")
+    stock_market.trade(symbol, quantity, price, trade_type)
     return {
-        "trade": "hello world"
+        "message": f"Traded for {symbol}: {quantity} shares at {price}, type: {trade_type}"
     }
 
 
-@bp.route('/volume_weighted_price', methods=['GET'])
+@bp.route("/volume_weighted_price", methods=["GET"])
 def volume_weighted_price():
     return {
         "volume_weighted_price": "hello world"
     }
 
 
-@bp.route('/gbce_index', methods=['GET'])
+@bp.route("/gbce_index", methods=["GET"])
 def gbce_index():
     return {
         "gbce_index": "hello world"
